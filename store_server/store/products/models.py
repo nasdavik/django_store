cfa82cf1f038323cbc74e499d3ffd6bcac32ebfc
nasdavik <120183618+nasdavik@users.noreply.github.com>
2023-08-12 15:23:12 +0500

    Add products/urls.py

--- store_server/store/products/models.py
+++ store_server/store/products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# models = таблицы
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} | Категория = {self.category.name}'
+
