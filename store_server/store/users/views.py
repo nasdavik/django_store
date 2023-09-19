08d179eb0c6110ae1205bbf673a8c8f87856d5ba
nasdavik <120183618+nasdavik@users.noreply.github.com>
2023-09-19 22:33:00 +0500

    Added logout

--- store_server/store/users/views.py
+++ store_server/store/users/views.py
@@ -54,3 +54,9 @@ def profile(request):
         'form': form
     }
     return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
